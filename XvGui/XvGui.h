c164b46a71a3db3224e36e7e3094ba18709f4cb0
Jason Snell <snell.jason@gmail.com>
2018-12-03 07:42:07 -0600

    Initial Commit

---
+++ XvGui/XvGui.h
@@ -0,0 +1,19 @@
+//
+//  XvGui.h
+//  XvGui
+//
+//  Created by Jason Snell on 12/3/18.
+//  Copyright © 2018 Jason Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvGui.
+FOUNDATION_EXPORT double XvGuiVersionNumber;
+
+//! Project version string for XvGui.
+FOUNDATION_EXPORT const unsigned char XvGuiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvGui/PublicHeader.h>
+
+
